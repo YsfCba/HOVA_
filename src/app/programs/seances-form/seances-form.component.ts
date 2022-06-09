e7a172b09649926a187861afb7b363884d18e9b0
youssef <youssefchouiba00@gmail.com>
2022-06-09 17:14:29 +0200

    ajouter du formulaire des seances

--- src/app/programs/seances-form/seances-form.component.ts
+++ src/app/programs/seances-form/seances-form.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-seances-form',
   templateUrl: './seances-form.component.html',
   styleUrls: ['./seances-form.component.scss'],
 })
+
 export class SeancesFormComponent implements OnInit {
 
   constructor() { }
 
+
+  public customFormatter(value: number) {
+    return `${value}%`;
+  }
+
   ngOnInit() {}
 
+  
 }
